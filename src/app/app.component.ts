5881c8cb88da6a6a6d1963be9e5da8c583857702
Gabriel Da Silva Almeida <gabrielalmeida2016@gmail.com>
2025-05-31 22:33:39 -0300

    esqueleto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as  AOS from 'aos';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'psicologia-pamela';
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init({
+    mirror: false,      // anima apenas ao descer
+    offset: 300,  
+    delay: 300            // atraso da animação
+      });
+      AOS.refresh();
+    }
+  }
 }
